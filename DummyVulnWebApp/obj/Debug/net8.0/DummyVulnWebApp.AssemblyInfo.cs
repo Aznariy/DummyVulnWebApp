0f89256539317843ff5606b85b9a3c338e8ce2dd
Aznariy <aznariy@gmail.com>
2024-08-29 11:39:12 -0400

    Committing secong change

--- DummyVulnWebApp/obj/Debug/net8.0/DummyVulnWebApp.AssemblyInfo.cs
+++ DummyVulnWebApp/obj/Debug/net8.0/DummyVulnWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DummyVulnWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee837681bb11c4e26bcc0681f573e22868ab0388")]
 [assembly: System.Reflection.AssemblyProductAttribute("DummyVulnWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DummyVulnWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
